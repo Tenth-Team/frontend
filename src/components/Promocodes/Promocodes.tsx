5f02fb2089fdbb1df337fcce511e0981e5aa4890
Dmitry Tokmak <dimka.listok@gmail.com>
2024-03-05 19:38:10 +0300

    fix eurl button

--- src/components/Promocodes/Promocodes.tsx
+++ src/components/Promocodes/Promocodes.tsx
@@ -1,18 +1,17 @@
 import { HeaderContent } from "../HeaderContent/HeaderContent"
 import { Content } from "../Content/Content"
 import TablePromocodes from "../TablePromocodes/TablePromocodes"
-import { Button } from "../../modules/ModalAddUser/FormAddUser/components/button"
-import style from './Promocodes.module.scss'
+import { Button } from "../../modules/ModalAddUser/FormAddUser/components/Button"
+import style from "./Promocodes.module.scss"
 
 const Promocodes = () => {
   return (
     <Content>
       <div className={style.headerArea}>
-      <HeaderContent>Промо-коды</HeaderContent>
-      <Button
-      primary
-      type="button"
-      >Добавить промо-код</Button>
+        <HeaderContent>Промо-коды</HeaderContent>
+        <Button primary type="button">
+          Добавить промо-код
+        </Button>
       </div>
       <TablePromocodes />
     </Content>
